81f3cdf0e3fd5ba080bfdec58211053295821732
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-25 07:47:08 -0300

    feat: add config module and typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,31 @@
 import { Module } from '@nestjs/common';
 import { GuardianModule } from './modules/guardian/guardian.module';
 import { PetModule } from './modules/pet/pet.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [PetModule, GuardianModule],
+  imports: [
+    PetModule,
+    GuardianModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        autoLoadEntities: true,
+        synchronize: true,
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+      }),
+    }),
+  ],
 })
 export class AppModule {}
