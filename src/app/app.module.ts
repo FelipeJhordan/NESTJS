500d75145e1ffc9f3a0f1537063c8f2fbd7002dd
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-25 13:48:04 -0300

    refactor: create layer app ( main )

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { GuardianModule } from './modules/guardian/guardian.module';
-import { PetModule } from './modules/pet/pet.module';
+import { GuardianModule } from '../modules/guardian/guardian.module';
+import { PetModule } from '../modules/pet/pet.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
