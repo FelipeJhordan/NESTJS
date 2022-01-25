c0ecbb81c52fbb413fa6fcc5d2d0d71ca9233178
Felipe Alves <felipejordan.alves@gmail.com>
2022-01-25 11:54:29 -0300

    chore: configure orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GuardianModule } from './modules/guardian/guardian.module';
 import { PetModule } from './modules/pet/pet.module';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
@@ -12,20 +12,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       isGlobal: true,
       expandVariables: true,
     }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        type: 'postgres',
-        autoLoadEntities: true,
-        synchronize: true,
-        host: configService.get('DB_HOST'),
-        port: configService.get('DB_PORT'),
-        username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_DATABASE'),
-      }),
-    }),
+    TypeOrmModule.forRoot(),
   ],
 })
 export class AppModule {}
